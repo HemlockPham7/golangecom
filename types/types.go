a354733279488257e2d4100f4fd56bb9f8e597af
HemlockPham7 <london1952hoang@gmail.com>
2024-10-27 23:46:42 +0700

    Feat: Check Out

--- types/types.go
+++ types/types.go
@@ -34,6 +34,8 @@ type ProductStore interface {
 	GetProducts() ([]*Product, error)
 	CreateProduct(Product) error
 	GetProductByID(id int) (*Product, error)
+	GetProductsByIDs(ids []int) ([]Product, error)
+	UpdateProduct(Product) error
 }
 
 type Product struct {
@@ -52,4 +54,35 @@ type CreateProductPayload struct {
 	Image       string  `json:"image"`
 	Price       float64 `json:"price" validate:"required"`
 	Quantity    int     `json:"quantity" validate:"required"`
+}
+
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"orderID"`
+	ProductID int       `json:"productID"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type CartCheckoutItem struct {
+	ProductID int `json:"productID"`
+	Quantity  int `json:"quantity"`
+}
+type CartCheckoutPayload struct {
+	Items []CartCheckoutItem `json:"items" validate:"required"`
 }
