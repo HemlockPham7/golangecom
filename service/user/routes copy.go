0c29132195a839d6cf3155abe522e4002561153f
HemlockPham7 <london1952hoang@gmail.com>
2024-10-27 11:05:40 +0700

    feat: add files

---
+++ service/user/routes copy.go
@@ -0,0 +1,72 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/HemlockPham7/golangecom/service/auth"
+	"github.com/HemlockPham7/golangecom/types"
+	"github.com/HemlockPham7/golangecom/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	store types.UserStore
+}
+
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/login", h.handlerLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
+}
+
+func (h *Handler) handlerLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	// check if the user exits
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+		return
+	}
+
+	// if it doens't we create the new user
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
